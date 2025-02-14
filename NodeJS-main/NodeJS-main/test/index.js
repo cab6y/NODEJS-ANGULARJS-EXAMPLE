e9147ae53c84d5c1a1c0c271bf419a71e3f20e8a
xaparge5 <84373344+xaparge5@users.noreply.github.com>
2025-02-14 23:59:38 +0300

    rev

--- NodeJS-main/NodeJS-main/test/index.js
+++ NodeJS-main/NodeJS-main/test/index.js
@@ -15,7 +15,7 @@ app.use(cors({
     methods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'],  
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,  // Allow cookies if necessary
-    origin: 'http://192.168.56.1:8081',  // Allow only this origin
+    origin: 'http://192.168.56.1:8080',  // Allow only this origin
 }));
 
 app.use(express.json()); 
