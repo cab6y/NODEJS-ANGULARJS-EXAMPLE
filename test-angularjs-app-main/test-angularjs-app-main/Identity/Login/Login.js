c6bfbd33c0bba854f6ea93f4fda64da9fae685ad
xaparge5 <84373344+xaparge5@users.noreply.github.com>
2025-02-15 23:30:28 +0300

    rev

--- test-angularjs-app-main/test-angularjs-app-main/Identity/Login/Login.js
+++ test-angularjs-app-main/test-angularjs-app-main/Identity/Login/Login.js
@@ -1,5 +1,5 @@
 angular.module('myApp')
-.controller('LoginController', function($scope, $http, API_BASE_URL) {
+.controller('LoginController', function($scope, $http, API_BASE_URL,IdentityService) {
     if (sessionStorage.length > 0) {
         window.location.href = "/";
     }
@@ -14,33 +14,10 @@ angular.module('myApp')
             username: $scope.user.username,
             password: $scope.user.password
         };
-
-        $http.post(API_BASE_URL + '/login', userData)
+        IdentityService.login(userData)
         .then(function(response) {
-            if (response.data !== false) {
-                console.log("**************" + response.data);
-                $scope.loggedIn = true;
-                $scope.user = JSON.stringify(response.data);
-                sessionStorage.user = JSON.stringify(response.data);
-                console.log(sessionStorage.user + "-------------");
-                window.location.href = "/";
-            } else  if (response.data == false) {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Giriş Başarısız!',
-                    text: 'Kullanıcı adı veya şifre hatalı.',
-                    confirmButtonText: 'Tamam'
-                });
-            }
+           
+            
         })
-        .catch(function(error) {
-            console.error('Hata:', error);
-            Swal.fire({
-                icon: 'error',
-                title: 'Bir hata oluştu!',
-                text: 'Sunucuya bağlanırken bir hata meydana geldi.',
-                confirmButtonText: 'Tamam'
-            });
-        });
     };
 });
--- test-angularjs-app-main/test-angularjs-app-main/Services/Identity/IdentityService.js
+++ test-angularjs-app-main/test-angularjs-app-main/Services/Identity/IdentityService.js
@@ -10,6 +10,36 @@ angular.module('myApp').service('IdentityService', function($http, API_BASE_URL)
             console.error('Hata:', error);
         });
     };
+
+    this.login = function(userData) {
+        $http.post(API_BASE_URL + '/login', userData)
+        .then(function(response) {
+            if (response.data !== false) {
+                console.log("**************" + response.data);
+                $scope.loggedIn = true;
+                $scope.user = JSON.stringify(response.data);
+                sessionStorage.user = JSON.stringify(response.data);
+                console.log(sessionStorage.user + "-------------");
+                window.location.href = "/";
+            } else  if (response.data == false) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Giriş Başarısız!',
+                    text: 'Kullanıcı adı veya şifre hatalı.',
+                    confirmButtonText: 'Tamam'
+                });
+            }
+        })
+        .catch(function(error) {
+            console.error('Hata:', error);
+            Swal.fire({
+                icon: 'error',
+                title: 'Bir hata oluştu!',
+                text: 'Sunucuya bağlanırken bir hata meydana geldi.',
+                confirmButtonText: 'Tamam'
+            });
+        });
+    };
     
 });
 
